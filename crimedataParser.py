3bf0d54eeca9c8465a9ed7575a30610b6cf50df8
armankal <armank@umd.edu>
2016-04-09 17:04:32 -0400

    Ok now it works

--- crimedataParser.py
+++ crimedataParser.py
@@ -3,17 +3,13 @@ crimedata = open("crimedata.txt")
 crimedataParsed = open("crimedataParsed.txt", 'w')
 
 dataIn = crimedata.read()
-breakStr = "</td></tr><tr><td rowspan=\"2\" valign=\"middle\">"
+breakStr = "</td></tr><tr><td"
 for i in range(len(dataIn)):
 	if(dataIn[i] == '>'):
-		k = i+1
-		breakLine = True
-		for j in range(len(breakStr)):
-			if(dataIn[i+j] != breakStr[j]):
-					breakLine = False
-		if(breakLine):
-			crimedataParsed.write('\n')
-		while(dataIn[k] != '<'):
-			crimedataParsed.write(dataIn[k])
-			k+=1
+		j = i+1
+		while(dataIn[j] != '<'):
+			crimedataParsed.write(dataIn[j])
+			j+=1
+	
+		crimedataParsed.write('\n')
 crimedata.close()
